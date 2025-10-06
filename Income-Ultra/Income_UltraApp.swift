68a0f5a415a2dfb6c2f13ffe8cd5f6d9dabcda36
Elisha Samuel Gyamfi <elishasamuel01@gmail.com>
2025-10-06 23:16:34 +0000

    Initial Commit

---
+++ Income-Ultra/Income_UltraApp.swift
@@ -0,0 +1,17 @@
+//
+//  Income_UltraApp.swift
+//  Income-Ultra
+//
+//  Created by Elisha Samuel Gyamfi on 10/6/25.
+//
+
+import SwiftUI
+
+@main
+struct Income_UltraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
