5955cdf8e50cdc5904e623bc0f70f26814c31644
Elisha Samuel Gyamfi <elishasamuel01@gmail.com>
2025-10-08 21:37:39 +0000

    Add main logic to calculate net and gross income

--- Income-Ultra/Core/IncomeViewModel.swift
+++ Income-Ultra/Core/IncomeViewModel.swift
@@ -13,6 +13,7 @@ import Factory
 @MainActor
 final class IncomeViewModel: ObservableObject {
     @Published var incomePickerValue: IncomePickerView.IncomeType = .salary
+    
     @Published var salaryInput: SalaryInput = .init()
     @Published var salaryOutput: SalaryOutput = .init()
     
@@ -22,6 +23,31 @@ final class IncomeViewModel: ObservableObject {
     @Injected(\.incomeStore) private var incomeStore
     @Injected(\.appInfoStore) private var appInfoStore
     
+    private var cancellables = Set<AnyCancellable>()
+    
+    init() {
+        setSubscribers()
+    }
+    
+    private func setSubscribers() {
+        $salaryInput
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] salaryInput in
+                guard let self else { return }
+                
+                salaryOutput = incomeStore.getSalaryOutput(for: salaryInput)
+            }
+            .store(in: &cancellables)
+        
+        $hourlyInput
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] hourlyInput in
+                guard let self else { return }
+                
+                hourlyOutput = incomeStore.getHourlyOutput(for: hourlyInput)
+            }
+            .store(in: &cancellables)
+    }
     var shouldShowOvertimeCheck: Bool {
         (hourlyInput.hoursPerWeek ?? 0) > hourlyInput.hoursPerWeekLimitForOvertime
     }
--- Income-Ultra/Stores/Income/IncomeLiveStore.swift
+++ Income-Ultra/Stores/Income/IncomeLiveStore.swift
@@ -8,5 +8,102 @@
 import Foundation
 
 final class IncomeLiveStore: IncomeStore {
+    func getSalaryOutput(for salaryInput: SalaryInput) -> SalaryOutput {
+        calculateSalaryOutput(for: salaryInput)
+    }
+    func getHourlyOutput(for hourlyInput: HourlyInput) -> HourlyOutput {
+        calculateHourlyOutput(for: hourlyInput)
+    }
+}
+
+private extension IncomeLiveStore {
+    func calculateSalaryOutput(for salaryInput: SalaryInput) -> SalaryOutput {
+        guard let yearlyGrossPay = salaryInput.yearlyGross else {
+            return .init()
+        }
+        
+        let weeksInYear: Double = 52
+        let monthsInYear: Double = 12
+        let hoursPerWeek: Double = 40
+        
+        let weeklyGrossPay: Double = yearlyGrossPay / weeksInYear
+        let biWeeklyGrossPay: Double = weeklyGrossPay * 2
+        let fourWeeklyGrossPay: Double = weeklyGrossPay * 4
+        let monthlyGrossPay: Double = yearlyGrossPay / monthsInYear
+        
+        let hourlyWage: Double = weeklyGrossPay / hoursPerWeek
+        
+        let takeHomePercentageDefault: Double = 83.00
+        let takeHomePercentage: Double = salaryInput.takeHomePercentage ?? takeHomePercentageDefault
+        let takeHomeMultiplier = takeHomePercentage / 100
+        
+        return .init(
+            hourlyWage: hourlyWage,
+            takeHomePay: .init(
+                incomeType: .takeHome,
+                weekly: weeklyGrossPay * takeHomeMultiplier,
+                biWeekly: biWeeklyGrossPay * takeHomeMultiplier,
+                fourWeekly: fourWeeklyGrossPay * takeHomeMultiplier,
+                monthly: monthlyGrossPay * takeHomeMultiplier,
+                yearly: yearlyGrossPay * takeHomeMultiplier
+            ),
+            grossPay: .init(
+                incomeType: .gross,
+                weekly: weeklyGrossPay,
+                biWeekly: biWeeklyGrossPay,
+                fourWeekly: fourWeeklyGrossPay,
+                monthly: monthlyGrossPay,
+                yearly: yearlyGrossPay
+            )
+        )
+    }
     
+    func calculateHourlyOutput(for hourlyInput: HourlyInput) -> HourlyOutput {
+        guard let hourlyWage = hourlyInput.hourlyWage,
+              let hoursPerWeek = hourlyInput.hoursPerWeek
+        else {
+            return .init()
+        }
+        
+        let weeksInYear: Double = 52
+        let monthsInYear: Double = 12
+        let hoursPerWeekLimitForOvertime: Double = 40
+        let overtimeRate: Double = 1.5
+        
+        let regularHours: Double = min(hoursPerWeek, hoursPerWeekLimitForOvertime)
+        let overtimeHours: Double = max(0, hoursPerWeek - hoursPerWeekLimitForOvertime)
+        let overtimeHourlyWage: Double = hourlyWage * overtimeRate
+        let shouldCalculateOvertime: Bool = (overtimeHours > 0 && hourlyInput.overtime)
+        
+        let weeklyGrossPay: Double = shouldCalculateOvertime ? (regularHours * hourlyWage) + (overtimeHours * overtimeHourlyWage)
+            : (hoursPerWeek * hourlyWage)
+        
+        let biWeeklyGrossPay: Double = weeklyGrossPay * 2
+        let fourWeeklyGrossPay: Double = weeklyGrossPay * 4
+        let yearlyGrossPay: Double = weeklyGrossPay * weeksInYear
+        let monthlyGrossPay: Double = yearlyGrossPay / monthsInYear
+        
+        let takeHomePercentageDefault: Double = 83.00
+        let takeHomePercentage: Double = hourlyInput.takeHomePercentage ?? takeHomePercentageDefault
+        let takeHomeMultiplier = takeHomePercentage / 100
+        
+        return .init(
+            takeHomePay: .init(
+                incomeType: .takeHome,
+                weekly: weeklyGrossPay * takeHomeMultiplier,
+                biWeekly: biWeeklyGrossPay * takeHomeMultiplier,
+                fourWeekly: fourWeeklyGrossPay * takeHomeMultiplier,
+                monthly: monthlyGrossPay * takeHomeMultiplier,
+                yearly: yearlyGrossPay * takeHomeMultiplier
+            ),
+            grossPay: .init(
+                incomeType: .gross,
+                weekly: weeklyGrossPay,
+                biWeekly: biWeeklyGrossPay,
+                fourWeekly: fourWeeklyGrossPay,
+                monthly: monthlyGrossPay,
+                yearly: yearlyGrossPay
+            )
+        )
+    }
 }
